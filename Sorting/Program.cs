3f0ef96a59206a7af98c3a3731c83182540b4a89
prinilb <prinilb@dunwoody.edu>
2022-05-19 10:43:15 -0500

    Add project files.

---
+++ Sorting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Sort
+{
+    static void bubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+
+    public static void Main()
+    {
+        int[] arr = { 34, 14, 25, 12, 10, 99 };
+        bubbleSort(arr);
+        Console.WriteLine("Sorted array: ");
+
+        int n = arr.Length;
+        for (int i = 0; i < n; ++i)
+            Console.Write(arr[i] + " ");
+        Console.WriteLine();
+    }
+}
